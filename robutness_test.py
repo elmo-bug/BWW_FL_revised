621d92a9b4b9c0df62bc31504d1bc2517462c06b
zengxl241799@163.com <ge53yon@mytum.de>
2024-04-17 15:29:44 +0800

    keep worker consistent

--- robutness_test.py
+++ robutness_test.py
@@ -154,80 +154,80 @@ if __name__=="__main__":
         df.to_csv(f"num_req_{len(x)}.csv") 
         # df.to_csv(f"cifar_num_req_{len(x)}.csv") 
      
-    '''(num of worker)/(num of requster) rate is constant change num of worker'''    
-    budget_each=[[28 for j in range(i) ] for i in range(4,11,2)]
-    num_per_type=[[5 for j in range(i) ] for i in range(4,11,2)] 
-    # for i in range(len(budget_each)):
-    #     print(budget_each[i],num_per_type[i])
-    groups={}
-    for index in range(len(num_per_type)):
-        print(f'num_per_type{num_per_type[index]},budget_each{budget_each[index]}')
-        flag=True
-        rep_set=[]
-        train_size=1000
-        test_size=2000
-        val_size=2000
-        batch_size=100
-        input=28*28
-        hidden=100
-        output=10
-        epochs=5
-        learning_rate=0.005
-        accuracy=[]
-        high=[]
-        low=[]
-        #same dataset for workers
-        for i in range(sum(num_per_type[index])):
-            accuracy.append(random.randint(1,10)/10)
-            high.append(3+(int)(accuracy[i]*10/3))
-            low.append(1+(int)(accuracy[i]*10/3)) 
-        # print(accuracy,len(accuracy))   
-        mode=['BMW_FL_s','BMW_FL_g','Ran_Pri_seperate','Ran_Pri_overall','RRAFL_seperate','RRAFL_overall']  
-        budget_all=sum(budget_each[index])
-        req_size=len(budget_each[index])
-        workers=[]
-        requester=[]
-        for i in range(sum(num_per_type[index])):
-            workers.append(Worker(input_size=input,hidden_size=hidden,output_size=output,accuracy=accuracy[i],data=data[i],ID=i,type_ID=get_type(num_per_type=num_per_type[index],ID=i),\
-                range_of_bid={"high":high[i],"low":low[i]},batch_size=batch_size,num_requesters=req_size,,mode='MNIST'))
-        for i in range(req_size):
-            requester.append(Requester(ID=i,budget=budget_each[index][i],workers=workers,num_per_type=num_per_type[index],num_requester=req_size,data=data[i],batch_size=batch_size,mode='MNIST'))   
-        req_set=Request_Set(workers=workers,requesters=requester,num_per_type=num_per_type[index],budget=budget_all)
-        #the reputation evaluationset fix across all divison of group
-        if flag:
-            for i in range(10):
-                req_set.run(mode='get_rep',size_of_selection=(int)(sum(num_per_type[index])/10))
-            rep_set=req_set.rep
-            req_set.reset_for_ALG()
-            flag=False    
-        req_set.rep=rep_set
-        for mod in mode:
-            print(mod,end='\n\n') 
-            for i in range(10):
-                print(f'round{i}')
-                req_set.run(mode=mod)
-                req_set.reset_for_ALG()
+    # '''(num of worker)/(num of requster) rate is constant change num of worker'''    
+    # budget_each=[[28 for j in range(i) ] for i in range(4,11,2)]
+    # num_per_type=[[5 for j in range(i) ] for i in range(4,11,2)] 
+    # # for i in range(len(budget_each)):
+    # #     print(budget_each[i],num_per_type[i])
+    # groups={}
+    # for index in range(len(num_per_type)):
+    #     print(f'num_per_type{num_per_type[index]},budget_each{budget_each[index]}')
+    #     flag=True
+    #     rep_set=[]
+    #     train_size=1000
+    #     test_size=2000
+    #     val_size=2000
+    #     batch_size=100
+    #     input=28*28
+    #     hidden=100
+    #     output=10
+    #     epochs=5
+    #     learning_rate=0.005
+    #     accuracy=[]
+    #     high=[]
+    #     low=[]
+    #     #same dataset for workers
+    #     for i in range(sum(num_per_type[index])):
+    #         accuracy.append(random.randint(1,10)/10)
+    #         high.append(3+(int)(accuracy[i]*10/3))
+    #         low.append(1+(int)(accuracy[i]*10/3)) 
+    #     # print(accuracy,len(accuracy))   
+    #     mode=['BMW_FL_s','BMW_FL_g','Ran_Pri_seperate','Ran_Pri_overall','RRAFL_seperate','RRAFL_overall']  
+    #     budget_all=sum(budget_each[index])
+    #     req_size=len(budget_each[index])
+    #     workers=[]
+    #     requester=[]
+    #     for i in range(sum(num_per_type[index])):
+    #         workers.append(Worker(input_size=input,hidden_size=hidden,output_size=output,accuracy=accuracy[i],data=data[i],ID=i,type_ID=get_type(num_per_type=num_per_type[index],ID=i),\
+    #             range_of_bid={"high":high[i],"low":low[i]},batch_size=batch_size,num_requesters=req_size,mode='MNIST'))
+    #     for i in range(req_size):
+    #         requester.append(Requester(ID=i,budget=budget_each[index][i],workers=workers,num_per_type=num_per_type[index],num_requester=req_size,data=data[i],batch_size=batch_size,mode='MNIST'))   
+    #     req_set=Request_Set(workers=workers,requesters=requester,num_per_type=num_per_type[index],budget=budget_all)
+    #     #the reputation evaluationset fix across all divison of group
+    #     if flag:
+    #         for i in range(10):
+    #             req_set.run(mode='get_rep',size_of_selection=(int)(sum(num_per_type[index])/10))
+    #         rep_set=req_set.rep
+    #         req_set.reset_for_ALG()
+    #         flag=False    
+    #     req_set.rep=rep_set
+    #     for mod in mode:
+    #         print(mod,end='\n\n') 
+    #         for i in range(10):
+    #             print(f'round{i}')
+    #             req_set.run(mode=mod)
+    #             req_set.reset_for_ALG()
     
-        #get avg accuracy for all rounds 
-        ac=req_set.accuracy
-        rep_per_round=req_set.rep_per_round
-        final_data={}
-        middle_data={}
-        for k,v in ac.items():
-            if len(v):
-                final_data[k]=(sum(v)/len(v),sum(rep_per_round[k])/(len(rep_per_round[k])))
-        for k,v in ac.items():
-            if len(v):
-                middle_data[k]=[(x,y) for x,y in zip(ac[k],rep_per_round[k])]          
-        groups[len(num_per_type[index])]=(final_data)
-        print(f'num_workers { groups[len(num_per_type[index])]}')   
-        print(f'middle data{middle_data}')    
-        df=pd.DataFrame.from_dict(groups)
-        df.to_csv("fashion_rate_div.csv")   
-        # df.to_csv("rate_div.csv")    
-        df=pd.DataFrame.from_dict(middle_data)
-        df.to_csv(f"fashion_rate_with_group_size_{len(num_per_type[index])}.csv") 
-        # df.to_csv(f"rate_with_group_size_{len(num_per_type[index])}.csv")  
+    #     #get avg accuracy for all rounds 
+    #     ac=req_set.accuracy
+    #     rep_per_round=req_set.rep_per_round
+    #     final_data={}
+    #     middle_data={}
+    #     for k,v in ac.items():
+    #         if len(v):
+    #             final_data[k]=(sum(v)/len(v),sum(rep_per_round[k])/(len(rep_per_round[k])))
+    #     for k,v in ac.items():
+    #         if len(v):
+    #             middle_data[k]=[(x,y) for x,y in zip(ac[k],rep_per_round[k])]          
+    #     groups[len(num_per_type[index])]=(final_data)
+    #     print(f'num_workers { groups[len(num_per_type[index])]}')   
+    #     print(f'middle data{middle_data}')    
+    #     df=pd.DataFrame.from_dict(groups)
+    #     # df.to_csv("fashion_rate_div.csv")   
+    #     df.to_csv("rate_div.csv")    
+    #     df=pd.DataFrame.from_dict(middle_data)
+    #     # df.to_csv(f"fashion_rate_with_group_size_{len(num_per_type[index])}.csv") 
+    #     df.to_csv(f"rate_with_group_size_{len(num_per_type[index])}.csv")  
         
        
     
